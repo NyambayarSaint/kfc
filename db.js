e7666f232e9f0d499127c0037a70afddda37ca1e
Nyambayar S <web-master@tavanbogd.com>
2020-07-23 16:57:34 +0800

    Save

--- db.js
+++ db.js
@@ -8,8 +8,17 @@ const PASSWORD = process.env.PASSWORD
 // const connectionString = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0-9crok.mongodb.net/kfc?retryWrites=true&w=majority`;
 // mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
 
+
 // LOCAL SETUP
-mongoose.connect('mongodb://127.0.0.1:27017/kfc', {
+// mongoose.connect('mongodb://127.0.0.1:27017/kfc', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// })
+
+
+// TB MONGO SERVER SETUP
+mongoose.connect(`mongodb://${USERNAME}:${PASSWORD}@192.168.10.82:27017/kfc`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
