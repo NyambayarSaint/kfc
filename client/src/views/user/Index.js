20ecb3680b98db9f8b91d2a06de17534fbec3065
Nyambayar S <web-master@tavanbogd.com>
2020-07-24 17:53:51 +0800

    Save

--- client/src/views/user/Index.js
+++ client/src/views/user/Index.js
@@ -1,29 +1,49 @@
-import React from "react";
-import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
-import Logout from "./operations/logout";
+import React, {useEffect, useState} from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import Header from "./layout/Header";
 import Sidebar from "./layout/Sidebar";
 import Main from "./layout/Main";
 import Integrations from './components/Integrations'
+import { FacebookProvider, Initialize } from 'react-facebook';
+import {UserContext} from './UserContext';
 
 const CoIndex = () => {
+
     const match = useRouteMatch()
+
     const menu = [
         {name: 'Dashboard', path: match.path, component: Main},
         {name: 'Integrations', path: match.path+'/integrations', component: Integrations},
     ]
+    const [authInfo, setAuthInfo] = useState({status: false, name: ''})
+    const [token, setToken] = useState(null)
+
+    const login = () => {
+        window.FB.login(function(response) {
+            console.log(response);
+            setToken(response.authResponse.accessToken)
+            window.FB.api('/me','GET',{"fields":"id,name"},(res2) => setAuthInfo({status: true, name: res2.name}));
+        }, {scope: 'pages_read_engagement'});
+    }
+
     return (
-        <div>
-            <Header />
-            <div className="container-fluid">
-                <div className="row">
-                    <Sidebar menu={menu} />
-                    <Switch>
-                        {menu.map((comp,i)=>i===0?<Route key={'rt'+i} exact path={comp.path} component={comp.component} />:<Route key={'rt'+i} path={comp.path} component={comp.component} />)}
-                    </Switch>
-                </div>
-            </div>
-        </div>
+        <FacebookProvider appId="2954126181364774">
+            <Initialize>
+                <UserContext.Provider value={{token, setToken}}>
+                    <Header user={authInfo} login={login} />
+                    {token ?
+                        <div className="container-fluid">
+                            <div className="row">
+                                <Sidebar menu={menu} />
+                                <Switch>
+                                    {menu.map((comp,i)=>i===0?<Route key={'rt'+i} exact path={comp.path} component={comp.component} />:<Route key={'rt'+i} path={comp.path} component={comp.component} />)}
+                                </Switch>
+                            </div>
+                        </div>
+                    : null}
+                </UserContext.Provider>
+            </Initialize>
+        </FacebookProvider>
     );
 };
 
--- client/src/views/user/layout/Header.js
+++ client/src/views/user/layout/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Logout from '../operations/logout';
+import {AiOutlineCheck} from 'react-icons/ai'
+import {FaCog} from 'react-icons/fa'
+import styled from 'styled-components'
 
-const Header = () => {
+const Header = ({user, login}) => {
     return (
-        <>
+        <Container>
             <div style={{ height: 56 }}></div>
             <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap pt-2 pb-2 shadow">
-                <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">User name</a>
+                {user.status ?
+                    <a className="navbar-brand col-sm-3 col-md-3 mr-0" href="#">{user.name} <span className="right"><AiOutlineCheck/></span></a>
+                :
+                    <a className="navbar-brand col-sm-3 col-md-3 mr-0" href="#" onClick={login}>Please authenticate! <span className="wrong"><FaCog/></span></a>
+                }
                 <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
                 <ul className="navbar-nav px-3">
                 <li className="nav-item text-nowrap">
@@ -14,8 +21,12 @@ const Header = () => {
                 </li>
                 </ul>
             </nav>
-        </>
+        </Container>
     );
 };
 
 export default Header;
+
+const Container = styled.div `
+    background:red;
+`;
